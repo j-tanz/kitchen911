9b949d8f885d46d880a4b38cd102aa3e42d30509
tanz <john.tansor@gmail.com>
2018-02-02 12:52:05 -0600

    adds addpends ajax calls into DOM

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
     var foodObj = {};
+    var recipeObj = {};
+
     $("#searchBttn").on("click", function(event){
         event.preventDefault();
-        var recipeSearch = $("#searchTerm").val();
+        var recipeSearch = $("#searchTerm").val().trim();
         var queryURL = "http://food2fork.com/api/search";
         var getURL = "http://food2fork.com/api/get";
-        // console.log("search", recipeSearch);
+
         $.ajax({
             url: 'https://cors-anywhere.herokuapp.com/' + queryURL,
             method: "GET",
@@ -20,9 +22,7 @@ $(document).ready(function(){
               lookUpId();       
         });
 
-function lookUpId() {
-        console.log("id", foodObj[recipeSearch].rID);
-        console.log(getURL);
+    function lookUpId() {
         $.ajax({
             url: 'https://cors-anywhere.herokuapp.com/' + getURL,
             method: 'GET',
@@ -31,8 +31,68 @@ function lookUpId() {
                 rId: foodObj[recipeSearch].rID
             }
           }).done(function(result){
-            console.log(result)
+              recipeObj[0] = {
+                  imgURL: JSON.parse(result).recipe.image_url,
+                  title: JSON.parse(result).recipe.title,
+                  ingredients: JSON.parse(result).recipe.ingredients
+              }
+console.log(recipeObj["0"]);
+            function addNewDiv() {
+                var newDiv = $("<div>");
+                newDiv.attr({
+                    "id": "Div" + recipeObj["0"].title
+                })
+                $("#resultsBox").append(newDiv);
+                
+                // var newDiv = document.createElement("div");
+
+                // $("#resultsBox").append("<div>").attr('id= div' + recipeObj["0"].title);
+                // $("#resultsBox").append("<div>");
+                // $(this).attr('id', "div_" + recipeObj[0].title);
+                return newDiv;
+            }
+            // console.log(newDiv);
+            addNewDiv();
+        
+
+            var recipeIMG = $("<img>");
+            var ingredientList = $("<ul>");
+            var recipeTitle = $("<h4>");
+            var ingredientArr = [];
+
+            // newDiv.attr()
+
+            recipeIMG.attr("src", recipeObj["0"].imgURL);
+            recipeIMG.attr("style","height: 75px; width: 75px; float: left" )
+            $("#resultsBox").append(recipeIMG);
+
+            recipeTitle.text(recipeObj["0"].title);
+            $("#resultsBox").append(recipeTitle);
+
+
+
+
+
+
+            // $.each(recipeObj[0].ingredients, function() {
+            //     for ( var i = 0; i < recipeObj[0].ingredients.length; i++ ) {
+            //     $('ul').append("<li><h6 class='ingredientsList'"> + recipeObj[0].ingredients[i] + "</h6></li>");
+            //     $("resultsBox").append(ingredientList);
+
+            //     }
+            // })
+            // console.log(recipeObj.imgURL);
+
+            // console.log(recipeObj);
+
+            //   console.log(recipeObj[JSON.parse(response).recipes[0].recipe_id]); 
           });
         }
     });
 });
+   
+
+
+
+
+
